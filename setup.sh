3b25d59a176a2637396fab7a40cf2b109fb21b45
Tim Hopper <tdhopper@gmail.com>
2015-05-09 20:59:33 -0400

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 virtualenv -p python3 ~/.virtualenvs/epistle
 pip install -r requirements.txt
 echo "use_env epistle" > .env
-echo "export S3_ACCESS_KEY=''" > .env
-echo "export S3_SECRET_KEY=''" > .env
+echo "export S3_ACCESS_KEY=''" >> .env
+echo "export S3_SECRET_KEY=''" >> .env
+echo "\n####################################"
 echo "Don't forget to set AWS keys in .env"
+echo "\n####################################"
